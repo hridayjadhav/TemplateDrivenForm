a2655f035c35d85a5875355cae30e59577407a15
Hriday <hridayjadhav789@gmail.com>
2023-08-02 14:33:30 +0530

    small template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  FormControl, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'formpractice';
+
+
+export class AppComponent implements OnInit{
+  title: any;
+
+  
+  getValue(f: any) { 
+    console.log(f);
+    
+  }
+
+  constructor(){}
+
+  ngOnInit(): void { }
+
+  onSubmit(f:NgForm){
+    console.log(f.value);
+
+  }
+
+
 }
